a385be7a9bd2954fff1fd5ae79852ba3ccd2a41e
박우림 <woorimprog@gmail.com>
2020-10-08 07:44:03 +0900

    [rasp/gTTS] 사운드 메세지가 저장될 전역변수 추가
    Signed-off-by: 박우림 <woorimprog@gmail.com>

--- hardware/raspberry/module/Env.py
+++ hardware/raspberry/module/Env.py
@@ -10,7 +10,7 @@ URL = str(os.environ['hangoURL'])
 SPEAK_OPTION = '-v ko+f3 -s 160 -p 95'
 
 # 전역 변수 선언
-sensings = {}           # 센싱된 데이터가 키와 값으로 저장될 딕셔너리 
+sensings = {}           # 센싱된 데이터가 키와 값으로 저장될 딕셔너리
 received_keys = set()   # 아두이노에게 전달 받을 변수명 집합
 
 # 라즈베리파이가 가공할 변수명 집합
@@ -18,12 +18,21 @@ basic_keys = {'success', 'sensed_position', 'sold_position', 'state'}
 
 # 서버에서 전달받은 음료 정보가 저장될 전역 변수
 drinks = {
-    'position' : [],
-    'name' : [],
-    'price' : [],
-    'count' : []
+    'position': [],
+    'name': [],
+    'price': [],
+    'count': []
 }
 
 # 멀티프로세싱에 사용될 변수
 pid = 0                 # 프로세스 아이디
 sensing_data = ''       # 현재 감지 중인 데이터
+
+# 음성 파일명 변수
+sound_msgs = {
+    'basic': '',
+    'position': {},
+    'sold': {},
+    'sold_out': {}
+}
+
