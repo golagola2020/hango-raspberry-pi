fdfe44e8e4716a249137f52b5f6e013ae756ca2e
parkwoorim <woorimprog@gmail.com>
2020-10-21 01:56:54 +0900

    [1.2.0] RPI_FILE_PATH 동적 경로 적용 #37
    Signed-off-by: parkwoorim <woorimprog@gmail.com>

--- raspberry/module/config.py
+++ raspberry/module/config.py
@@ -5,7 +5,7 @@
 import os
 
 # 초기 세팅
-PORT = '/dev/ttyACM0'
+PORT = '/dev/ttyACM1'
 USER_ID = 'rltn123'
 SERIAL_NUMBER = '20200814042555141'
 
@@ -13,10 +13,10 @@ SERIAL_NUMBER = '20200814042555141'
 SPEAK_OPTION = '-v ko+f3 -s 160 -p 95'
 
 # 라즈베리파이가 가공할 변수명 집합
-BASIC_KEYS = {'success', 'sensed_position', 'sold_position'}
+BASIC_KEYS = {'success', 'sensed_position', 'sold_position', 'duplicate'}
 
 # RPi File Path
-RPI_FILE_PATH = '/home/pi/hango-hardware/hardware/raspberry'
+RPI_FILE_PATH = f"{os.path.dirname(os.path.realpath('main.py'))}"
 
 
 
