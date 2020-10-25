898f51ebad46703aef96b1b375ef7661169f7fc8
박우림 <woorimprog@gmail.com>
2020-10-25 23:15:19 +0900

    [debug] API 환경 변수 미등록시 원인 로그 출력 후 종료되도록 구현
    Signed-off-by: 박우림 <woorimprog@gmail.com>

--- raspberry/module/API.py
+++ raspberry/module/API.py
@@ -1,7 +1,13 @@
 import os       # 시스템 모듈
 
-# 데이터 요청 Domain 선언
-URL = str(os.environ['HANGO_URL'])
+# 환경 변수명 지정
+ENV = 'HANGO_URL'
+try:
+  # URL 환경 변수 불러오기
+  URL = str(os.environ[ENV])
+except:
+  print(f"'{ENV}'는 존재하지 않는 환경 변수입니다.\n먼저, 환경 변수를 등록해주십시오.")
+  exit(1)
 
 # 서버 요청 및 응답 API
 READ_DRINKS_PATH = URL + '/rasp/drink/read'
