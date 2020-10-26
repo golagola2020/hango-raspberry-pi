154c7937e1601c134788680ecb51c948dba74abe
parkwoorim <woorimprog@gmail.com>
2020-10-26 18:47:08 +0900

    [v1.3.0] Gspeak 클래스의 origin_price 리스트 인덱스 설정 오류 해결
    Signed-off-by: parkwoorim <woorimprog@gmail.com>

--- raspberry/module/Speak.py
+++ raspberry/module/Speak.py
@@ -78,7 +78,7 @@ class Gspeak:
             # 사운드로 만들어지지 않은 음료수가 있다면 실행
             if drink_name not in self.sound_msgs["position"] or drinks["price"][idx] != self.origin_price[idx]:
                 # 자판기의 모든 음료 정보를 하나의 문자열로 병합
-                print(f'변경된 음료 : {drink_name}\n{drink_name} 음성 파일을 생성합니다...')
+                print(f'{drink_name} 음성 파일을 생성합니다...')
                 names = ""
                 for i, name in enumerate(drinks["name"]):
                     names += f"{str(i+1)}번 {name} "
@@ -88,7 +88,7 @@ class Gspeak:
                 self.save_sound('basic', 'basic', self.sound_msgs["basic"]["basic"])
                 # 손이 음료를 향해 위치한 상태 메세지
                 self.sound_msgs["position"][drinks["name"][idx]] = f"{drinks['position'][idx]}번. {drinks['name'][idx]}. {str(drinks['price'][idx])}원입니다아."
-                self.origin_price[i] = drinks['price'][idx]     # 수정된 음료 가격 저장
+                self.origin_price[idx] = drinks['price'][idx]     # 수정된 음료 가격 저장
                 self.save_sound('position', drinks["name"][idx], self.sound_msgs["position"][drinks["name"][idx]])
                 # 음료수가 팔린 상태
                 self.sound_msgs["sold"][drinks["name"][idx]] = f"{drinks['position'][idx]}번. {drinks['name'][idx]} 선택. 맛있게 드시고 즐거운 하루 되십시오."
